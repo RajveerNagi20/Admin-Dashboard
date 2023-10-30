1adba4fd455c5bdc239b34e551e0875209a67db4
RajveerNagi20 <149399415+RajveerNagi20@users.noreply.github.com>
2023-10-30 20:16:39 +0530

    Add files via upload

---
+++ Demotest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demotest;
+
+/**
+ *
+ * @author HP
+ */
+public class Demotest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
